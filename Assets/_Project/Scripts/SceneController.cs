4ebb9497445443e30bded5e558dc09204b6478bf
Felipe Cabedo <felipe.cabedo@gojira.tv>
2022-05-26 03:09:07 +0100

    Added a restart button when player dies.

--- Assets/_Project/Scripts/SceneController.cs
+++ Assets/_Project/Scripts/SceneController.cs
@@ -5,7 +5,9 @@ namespace pepipe.DeathRun
 {
     public class SceneController : MonoBehaviour {
         [SerializeField] PlayerController m_Player;
+        [SerializeField] string m_Version = "0.0.01";
 
         public PlayerController Player => m_Player;
+        public string Version => m_Version;
     }
 }
--- Assets/_Project/Scripts/UI/UIController.cs
+++ Assets/_Project/Scripts/UI/UIController.cs
@@ -1,24 +1,40 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace pepipe.DeathRun.UI
 {
     public class UIController : MonoBehaviour {
         [SerializeField] TextMeshProUGUI m_ScoreText;
+        [SerializeField] Button m_RestartBtn;
+        [SerializeField] TextMeshProUGUI m_Version;
 
         SceneController _sceneController;
 
         void Start() {
             _sceneController = FindObjectOfType<SceneController>();
             _sceneController.Player.Score += UpdateScoreText;
+            _sceneController.Player.Dying += OnPlayerDead;
+            m_Version.text = _sceneController.Version;
         }
 
         void OnDisable() {
+            if (_sceneController == null || _sceneController.Player == null) return;
+            
             _sceneController.Player.Score -= UpdateScoreText;
+            _sceneController.Player.Dying -= OnPlayerDead;
         }
 
         void UpdateScoreText(int playerPos) {
             m_ScoreText.text = playerPos + "m";
+        }
+
+        void OnPlayerDead() {
+            m_RestartBtn.onClick.AddListener(() => {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            });
+            m_RestartBtn.gameObject.SetActive(true);
         }
     }
 }
